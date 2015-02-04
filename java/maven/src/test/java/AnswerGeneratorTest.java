4eb8ed5cc6ac852d2846c1b354cb112402fab438
HeWanli <806635720@qq.com>
2015-02-04 17:25:37 +0800

    finish 2015-02-04 17:25

--- java/maven/src/test/java/AnswerGeneratorTest.java
+++ java/maven/src/test/java/AnswerGeneratorTest.java
@@ -33,7 +33,7 @@ public class AnswerGeneratorTest {
         String digit = new NumberGenerator(new Random()).generate();
         for (int i = 0; i < digit.length(); i++) {
             char ch = digit.charAt(i);
-            assertThat(digit.indexOf(i)).isEqualTo(digit.lastIndexOf(ch));
+            assertThat(digit.indexOf(i)).isNotEqualTo(digit.lastIndexOf(ch));
         }
     }
 
@@ -48,6 +48,45 @@ public class AnswerGeneratorTest {
 
         NumberGenerator numberGenerator = new NumberGenerator(random);
         HashSet<String> digits = new HashSet<String>();
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        assertThat(digits.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void should_be_able_to_repeat_out_of_3_times(){
+        Random random = mock(Random.class);
+        given(random.nextInt(10))
+                .willReturn(1,2,3,4)
+                .willReturn(2,3,4,5)
+                .willReturn(3,4,5,6)
+                .willReturn(1,2,3,4)
+                .willReturn(4,5,6,7);
+
+        NumberGenerator numberGenerator = new NumberGenerator(random);
+        HashSet<String> digits = new HashSet<String>();
+
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        digits.add(numberGenerator.generate());
+        assertThat(digits.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void tst_up_method() {
+        Random random = mock(Random.class);
+        given(random.nextInt(10))
+                .willReturn(1,2,3,4)
+                .willReturn(2,3,4,5)
+                .willReturn(3,4,5,6)
+                .willReturn(4,5,6,7);
+
+        NumberGenerator numberGenerator = new NumberGenerator(random);
+        HashSet<String> digits = new HashSet<String>();
+
         digits.add(numberGenerator.generate());
         digits.add(numberGenerator.generate());
         digits.add(numberGenerator.generate());
